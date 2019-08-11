5966e45e3b1819c89078594887eb9563277248c2
Terekhov Alexander <terechovalexanderr@gmail.com>
2019-08-11 15:27:12 +0300

    Create mult.asm

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,41 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@R0
+D=M
+@value
+M=D // value = R0
+
+@R1
+D=M
+@i
+M=D // i = R1
+
+@result
+M=0 // result = 0
+
+(LOOP)
+@i
+D=M
+@STOP
+D;JEQ
+
+@value
+D=M
+@result
+M=D+M // result = result + value
+@i
+M=M-1 // n = n - 1
+@LOOP
+0;JMP
+
+
+(STOP)
+@result
+D=M
+@R2
+M=D // RAM[2] = result
+
+(END)
+@END
+0;JMP
